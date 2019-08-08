3480d6311b925e570004efc788f6f145d0b94092
Manuel Morales <bechy10@live.com.mx>
2019-08-07 22:23:17 -0500

    Login Iniciado

--- MauLabs/FrmLogin.Designer.vb
+++ MauLabs/FrmLogin.Designer.vb
@@ -19,15 +19,34 @@ Partial Class FrmLogin
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(286, 465)
+        Me.Panel1.TabIndex = 0
+        '
+        'Timer1
+        '
         '
         'FrmLogin
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(655, 465)
+        Me.Controls.Add(Me.Panel1)
         Me.Name = "FrmLogin"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Timer1 As Timer
 End Class
--- MauLabs/FrmLogin.vb
+++ MauLabs/FrmLogin.vb
@@ -1,3 +1,19 @@
 ﻿Public Class FrmLogin
 
+
+    Private Sub FrmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim num As Integer = 10
+        If num < 10 Then
+            num = num + 1
+        Else
+            Timer1.Stop()
+            While Panel1.Width < 286
+                Panel1.Width += 1
+            End While
+        End If
+    End Sub
 End Class
