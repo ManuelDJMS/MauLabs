7864d904f535e6968a9c611c3e3dbe5e93f98d83
Manuel Morales <bechy10@live.com.mx>
2019-08-15 23:18:23 -0500

    Update FrmLogin.Designer.vb

--- MauLabs/FrmLogin.Designer.vb
+++ MauLabs/FrmLogin.Designer.vb
@@ -26,8 +26,11 @@ Partial Class FrmLogin
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.Panel2 = New System.Windows.Forms.Panel()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextUsuario = New System.Windows.Forms.TextBox()
+        Me.TextPassword = New System.Windows.Forms.TextBox()
         Me.Panel1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
         '
         'Panel1
@@ -56,7 +59,9 @@ Partial Class FrmLogin
         'Panel2
         '
         Me.Panel2.BackgroundImage = Global.MauLabs.My.Resources.Resources._2
-        Me.Panel2.Location = New System.Drawing.Point(378, 83)
+        Me.Panel2.Controls.Add(Me.TextPassword)
+        Me.Panel2.Controls.Add(Me.TextUsuario)
+        Me.Panel2.Location = New System.Drawing.Point(378, 123)
         Me.Panel2.Name = "Panel2"
         Me.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.Yes
         Me.Panel2.Size = New System.Drawing.Size(316, 227)
@@ -66,11 +71,33 @@ Partial Class FrmLogin
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Bookman Old Style", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(405, 36)
+        Me.Label1.Location = New System.Drawing.Point(409, 69)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(257, 35)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Inicio de Sesión"
+        '
+        'TextUsuario
+        '
+        Me.TextUsuario.BackColor = System.Drawing.Color.FromArgb(CType(CType(230, Byte), Integer), CType(CType(230, Byte), Integer), CType(CType(230, Byte), Integer))
+        Me.TextUsuario.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextUsuario.Font = New System.Drawing.Font("Tahoma", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextUsuario.Location = New System.Drawing.Point(68, 26)
+        Me.TextUsuario.Name = "TextUsuario"
+        Me.TextUsuario.Size = New System.Drawing.Size(220, 29)
+        Me.TextUsuario.TabIndex = 0
+        Me.TextUsuario.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'TextPassword
+        '
+        Me.TextPassword.BackColor = System.Drawing.Color.FromArgb(CType(CType(230, Byte), Integer), CType(CType(230, Byte), Integer), CType(CType(230, Byte), Integer))
+        Me.TextPassword.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextPassword.Font = New System.Drawing.Font("Tahoma", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextPassword.Location = New System.Drawing.Point(68, 87)
+        Me.TextPassword.Name = "TextPassword"
+        Me.TextPassword.Size = New System.Drawing.Size(220, 29)
+        Me.TextPassword.TabIndex = 1
+        Me.TextPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
         'FrmLogin
         '
@@ -87,6 +114,8 @@ Partial Class FrmLogin
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -97,4 +126,6 @@ Partial Class FrmLogin
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Panel2 As Panel
     Friend WithEvents Label1 As Label
+    Friend WithEvents TextUsuario As TextBox
+    Friend WithEvents TextPassword As TextBox
 End Class
